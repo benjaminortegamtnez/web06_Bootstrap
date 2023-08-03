628a0b561add36fbc35e080aefd83e1ca8ae8a4c
Benjamín Ortega´ <benjaminortega.mtnez@gmail.com>
2023-08-03 17:25:40 -0600

    Patricio sigue coloreando

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,12 @@ console.log("Página en construcción con Patricio Estrella");
 function clickPatricio() {
     alert("La mayonessa es un instrumento?");
 }
+
+function changeColor( elemntHTML, color) {
+    /* alert("Que emoción, voy a cambiar mi color") */
+    console.log(elemntHTML);
+    console.log(color);
+
+    elemntHTML.style.color = color;
+
+}
