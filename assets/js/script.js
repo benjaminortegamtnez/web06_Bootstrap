6177f1985919e2bcb1bc72988c7f9f9180642d71
Benjamín Ortega´ <benjaminortega.mtnez@gmail.com>
2023-08-04 12:15:19 -0600

    Version final patricio

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,34 @@ function changeColor( elemntHTML, color) {
     elemntHTML.style.color = color;
 
 }
+
+function changeColorWithPrompt( elementHtml) {
+    const color = prompt("Dime el color en inglés", "yellow");
+     changeTextOfUserColor ( elementHtml, color );
+    changeColor( elementHtml, color );
+}
+
+function changeTextOfUserColor ( element, color ) {
+    element.innerHTML = "<h3>Ahora soy <em>" + color + "</em> </h3>";
+}
+
+function resetColor () {
+    const refRedColor = document.getElementById("red-color");
+    const refGreenColor = document.getElementById("green-color");
+    const refPurpleColor = document.getElementById("purple-color");
+    const refUserColor = document.getElementById("user-color");
+
+    changeColor(refRedColor, 'Black');
+    changeColor(refGreenColor, 'Black');
+    changeColor(refPurpleColor, 'Black');
+    changeColor(refUserColor, 'Black');
+    changeTextOfUserColor( refUserColor, 'black');
+
+}
+
+function changeName() {
+    const userName = prompt("agrega tu nombre CHOLO", "Big Smoke");
+    const refGretting = document.getElementById("gretting");
+    refGretting.innerHTML = `Hola ${userName}, la clika te respalda`;
+
+}
